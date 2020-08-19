18f7241adabd0fd174986e9e916cc5728c045525
13 <yishan13@25sprout.com>
2020-08-19 11:22:06 +0800

    remove page scroll switch

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -36,20 +36,6 @@
 
   //===== Section Menu Active
 
-  var scrollLink = $(".page-scroll");
-  // Active link switching
-  $(window).scroll(function () {
-    var scrollbarLocation = $(this).scrollTop();
-
-    scrollLink.each(function () {
-      var sectionOffset = $(this.hash).offset().top - 73;
-
-      if (sectionOffset <= scrollbarLocation) {
-        $(this).parent().addClass("active");
-        $(this).parent().siblings().removeClass("active");
-      }
-    });
-  });
 
   // Parallaxmouse js
 
